2ca6a3afdc86821b0c24c31329450a10df88d837
Sage <salsaeed999@gmail.com>
2023-02-09 01:21:15 -0500

    return json data for the index and about routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Index Page"
+    data = {
+        "page name": "index page",
+        "page route": "/"
+    }
+
+    return jsonify(data)
 
 
-@app.route("/hello")
-def hello():
-    return "Hello, World"
+@app.route("/about")
+def about():
+    data = {
+        "page name": "about page",
+        "page route": "/about"
+    }
+
+    return jsonify(data)
 
