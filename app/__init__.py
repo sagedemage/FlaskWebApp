52e32d963c4787c4d3850df3bc6752487d2bd53b
Salmaan Saeed <salsaeed999@gmail.com>
2023-02-11 19:18:00 -0500

    setup mysql database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,24 @@
+import os
+
 from flask import Flask, jsonify
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
+
+DB_USERNAME = "root"
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = "notebook2"
+DB_HOST = "127.0.0.1"
+DB_PORT = "3306"
+
+DB_URL = 'mysql://' + DB_USERNAME + ':' + DB_PASSWORD + '@' + DB_HOST + ':' + DB_PORT + '/' + DB_NAME
+
+# 'mysql://username:password@host:port/database_name'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 
 
 @app.route("/")
