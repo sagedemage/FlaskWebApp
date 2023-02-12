828acec91ddda55afb47f183e6d5332cacee2484
Salmaan Saeed <salsaeed999@gmail.com>
2023-02-11 21:39:49 -0500

    created create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,26 @@
-import os
-
-from flask import Flask, jsonify
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-app = Flask(__name__)
-
-DB_USERNAME = "root"
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = "notebook2"
-DB_HOST = "127.0.0.1"
-DB_PORT = "3306"
-
-DB_URL = 'mysql://' + DB_USERNAME + ':' + DB_PASSWORD + '@' + DB_HOST + ':' + DB_PORT + '/' + DB_NAME
-
-# 'mysql://username:password@host:port/database_name'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+from flask import Flask
+from app.db import db, create_db
+from app.config import mysql_db_url
+from app.routes import rest_api_routes
 
 
-@app.route("/")
-def index():
-    data = {
-        "page name": "index page",
-        "page route": "/"
-    }
+def create_app():
+    app = Flask(__name__)
 
-    return jsonify(data)
+    app.config["SQLALCHEMY_DATABASE_URI"] = mysql_db_url()
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.register_blueprint(rest_api_routes)
+
+    db.init_app(app)
+
+    #with app.app_context():
+    #db.create_all()
+
+    return app
 
 
-@app.route("/about")
-def about():
-    data = {
-        "page name": "about page",
-        "page route": "/about"
-    }
 
-    return jsonify(data)
+
+
+
 
