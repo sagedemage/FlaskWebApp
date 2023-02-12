973e93924af520b9d3c9ae3565b02b5cb4e10e76
Salmaan Saeed <salsaeed999@gmail.com>
2023-02-11 23:38:25 -0500

    defined the user and note models

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,7 +1,21 @@
 from app.db import db
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(20))
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(100))
+    username = Column(String(100))
+    password = Column(String(100))
+    note = relationship("Note", backref="user")
+
+
+class Note(db.Model):
+    __tablename__ = "note"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    description = Column(String(100))
+    user_id = Column(Integer, ForeignKey("user.id"))
 
