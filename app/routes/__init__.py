019ec2f0c4a5a54c5ded3b4dcc68eb884682bda2
Sage <salsaeed999@gmail.com>
2023-02-13 23:40:26 -0500

    improved register api and added the login api

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -25,6 +25,7 @@ def about():
 
     return jsonify(data)
 
+
 @rest_api_routes.route("/api/register", methods=["POST"])
 def register():
     data = json.loads(request.data)
@@ -33,14 +34,50 @@ def register():
     username = data["username"]
     password = data["password"]
 
+    email_exists = User.query.filter_by(email=email).first()
+    username_exists = User.query.filter_by(username=username).first()
+
     user = User(email=email, username=username, password=password)
 
-    db.session.add(user)
-    db.session.commit()
-    
-    data = {
-        "page name": "about page",
-        "page route": "/about"
-    }
+    msg = {}
 
-    return jsonify(data)
+    if email_exists is not None:
+        msg["err_msg"] = "email exists"
+    elif username_exists is not None:
+        msg["err_msg"] = "username exists"
+    else:
+        db.session.add(user)
+        db.session.commit()
+        msg["msg"] = "success"
+
+    return jsonify(msg)
+
+
+@rest_api_routes.route("/api/login", methods=["POST"])
+def login():
+    data = json.loads(request.data)
+
+    username = data["username"]
+    password = data["password"]
+
+    email_exists = User.query.filter_by(email=username).first()
+    username_exists = User.query.filter_by(username=username).first()
+
+    msg = {}
+
+    if email_exists is not None:
+        if email_exists.check_password(password):
+            msg["msg"] = "success"
+        else:
+            msg["err_msg"] = "wrong credentials"
+    elif username_exists is not None:
+        if username_exists.check_password(password):
+            msg["msg"] = "success"
+        else:
+            msg["err_msg"] = "wrong credentials"
+    elif email_exists is None:
+        msg["err_msg"] = "wrong credentials"
+    elif username_exists is None:
+        msg["err_msg"] = "wrong credentials"
+
+    return jsonify(msg)
