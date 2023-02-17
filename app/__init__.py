e8b8404237a3d74696355c68ac650fa8113ca5ff
Sage <salsaeed999@gmail.com>
2023-02-16 21:51:36 -0500

    created a note blueprint for note routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.config import mysql_db_url
 from app.routes.auth import auth_routes
+from app.routes.note import note_routes
 from app.db import db
 from flask_migrate import Migrate
 from app.db.models import *
@@ -14,6 +15,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = mysql_db_url()
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.register_blueprint(auth_routes)
+    app.register_blueprint(note_routes)
 
     db.init_app(app)
     migrate.init_app(app, db)
