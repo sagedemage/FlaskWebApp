dc1974d94b551eb8f5c9a31e5c64cee2968ab764
Sage <salsaeed999@gmail.com>
2023-02-18 02:42:04 -0500

    the main app switches from using mysql or sqlite database depending on the value of environmental variable called testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from app.config import mysql_db_url
+from app.config import mysql_db_url, sqlite_url
 from app.routes.auth import auth_routes
 from app.routes.note import note_routes
-from app.db import db
+from app.db import db, create_db
 from flask_migrate import Migrate
 from app.db.models import *
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 migrate = Migrate()
 
@@ -12,16 +16,31 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = mysql_db_url()
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    if os.getenv("TESTING") == "false":
+        app.config["TESTING"] = False
+    elif os.getenv("TESTING") == "true":
+        app.config["TESTING"] = True
+
+    if app.config["TESTING"]:
+        app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url()
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    elif not app.config["TESTING"]:
+        app.config["SQLALCHEMY_DATABASE_URI"] = mysql_db_url()
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     app.register_blueprint(auth_routes)
     app.register_blueprint(note_routes)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    with app.app_context():
-        db.create_all()
+    if app.config["TESTING"]:
+        with app.app_context():
+            create_db()
+            db.create_all()
+    elif not app.config["TESTING"]:
+        with app.app_context():
+            db.create_all()
 
     return app
 
